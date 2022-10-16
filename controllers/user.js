109617df9db197dc2c071f05e42edd0dc54eb269
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-10-16 14:54:57 +0300

    fix 404 to 400, added 404 to user finder

--- controllers/user.js
+++ controllers/user.js
@@ -24,10 +24,14 @@ module.exports.getProfile = async (req, res) => {
   try {
     const { id } = req.params;
     const response = await User.findById(id);
+    if (!response) {
+      res.status(404).send({ message: 'Валидация не пройдена, проверьте правильность введённых данных!' });
+      return;
+    }
     res.send(response);
   } catch (error) {
     if (error.name === 'CastError') {
-      res.status(404).send({ message: 'Пользователя с таким ID не существует' });
+      res.status(400).send({ message: 'Пользователя с таким ID не существует' });
       return;
     }
     res.status(500).send({ message: 'Что-то пошло не так :(' });
