20b8af0ddb90e13bd9b6e2495f03bef76d38497c
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-10-31 23:22:28 +0300

    fix joi errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -15,12 +15,10 @@ module.exports.validateSignup = celebrate({
   body: Joi.object().keys({
     name: Joi
       .string()
-      .required()
       .min(2)
       .max(30),
     about: Joi
       .string()
-      .required()
       .min(2)
       .max(30),
 
