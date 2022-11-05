a3434842bfb786f37117178790dddc6c3638792b
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-11-05 22:44:42 +0300

    123

--- cors/cors.js
+++ cors/cors.js
@@ -13,9 +13,6 @@ module.exports.cors = (req, res, next) => {
   }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
