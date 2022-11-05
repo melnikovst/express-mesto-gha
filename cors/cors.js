fcbb7231445209cee77c084b3e7d58b50564a21a
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-11-05 23:58:04 +0300

    123

--- cors/cors.js
+++ cors/cors.js
@@ -9,6 +9,7 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports.cors = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
+  res.header('Access-Control-Allow-Credentials', true);
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -19,6 +20,5 @@ module.exports.cors = (req, res, next) => {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-  res.header('Access-Control-Allow-Credentials', true);
   return next();
 };
