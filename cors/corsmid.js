76bb1c837e745c3ff9f21c656f3900cc93c3a854
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-11-06 15:43:59 +0300

    fixes

--- cors/corsmid.js
+++ cors/corsmid.js
@@ -1,8 +1,6 @@
 const allowedCors = [
   'https://melnikovst.mesto.nomoredomains.icu',
   'http://melnikovst.mesto.nomoredomains.icu',
-  'https://localhost:3000',
-  'http://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
@@ -12,16 +10,11 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
-  // для отправки credentials в fetch
   res.header('Access-Control-Allow-Credentials', true);
-  // простой CORS
-  // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  // предварительный запрос
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
