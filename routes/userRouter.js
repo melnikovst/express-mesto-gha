ff70cee5a1c62f35576fd886049a5a006bdf5a95
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-11-07 23:01:21 +0300

    feat signout rote

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 const userRouter = require('express').Router();
 const {
-  getProfiles, getProfile, updateProfile, updateAvatar, me,
+  getProfiles, getProfile, updateProfile, updateAvatar, me, signout,
 } = require('../controllers/user');
 const { validateProfile, validateAvatar, validateIds } = require('../middlewares/error');
 
 userRouter.get('/users/me', me);
+userRouter.get('/signout', signout);
 userRouter.get('/users', getProfiles);
 userRouter.get('/users/:id', validateIds, getProfile);
 userRouter.patch('/users/me', validateProfile, updateProfile);
