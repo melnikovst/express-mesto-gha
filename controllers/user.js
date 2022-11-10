81594637fd52a25acc1769dc0b42b8ca8f13517f
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-11-10 09:29:51 +0300

    3

--- controllers/user.js
+++ controllers/user.js
@@ -28,15 +28,7 @@ module.exports.postProfile = async (req, res, next) => {
     const response = await User.create({
       _id, name, about, avatar, email, password: hashedPassword,
     });
-    const key = jwt.sign({ _id: response._id }, JWT_SECRET, {
-      expiresIn: '7d',
-    });
-    res.cookie('jwt', key, {
-      sameSite: 'None',
-      secure: true,
-      maxAge: 7777777,
-      httpOnly: true,
-    }).send(response);
+    res.send(response);
   } catch (error) {
     if (error.name === 'ValidationError') {
       return next(new BadRequest('Валидация не пройдена, проверьте правильность введённых данных!'));
@@ -95,15 +87,7 @@ module.exports.updateAvatar = async (req, res, next) => {
 module.exports.me = async (req, res, next) => {
   try {
     const me = await User.findOne({ _id: req.user._id });
-    const key = jwt.sign({ _id: me._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
-      expiresIn: '7d',
-    });
-    res.cookie('jwt', key, {
-      sameSite: 'None',
-      secure: true,
-      maxAge: 7777777,
-      httpOnly: true,
-    }).send(me);
+    res.send(me);
   } catch (error) {
     next(error);
   }
